f9e527c79408926ceaf6ecb3b9b6d6463128d0ec
Freddy Bijanja <freddybijanja31@gmail.com>
2025-12-05 10:06:19 +0200

    add authentication login

---
+++ src/auth.js
@@ -0,0 +1,62 @@
+import { auth, provider } from "./firebase-config.js";
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+// ========== SELECTORS ==========
+const loginEmail = document.querySelector('#loginForm input[type="email"]');
+const loginPassword = document.querySelector('#loginForm input[type="password"]');
+const loginBtn = document.querySelector('#loginForm button');
+
+const googleBtn = document.querySelector('#loginForm .fa-google-plus').parentElement;
+
+const registerForm = document.querySelector('#registerForm');
+const registerInputs = registerForm.querySelectorAll("input");
+const registerBtn = registerForm.querySelector("button");
+
+// ========== LOGIN ==========
+loginBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    try {
+        await signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value);
+
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+});
+
+// ========== REGISTER ==========
+registerBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const fullName = registerInputs[0].value;
+    const email = registerInputs[1].value;
+    const password = registerInputs[2].value;
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Account created!");
+        document.getElementById("registerTab").click(); // Go back to login
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+});
+
+// ========== GOOGLE LOGIN ==========
+googleBtn.addEventListener("click", async () => {
+    try {
+        await signInWithPopup(auth, provider);
+        alert("Google Login successful!");
+        window.location.href = "dashboard.html";
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+});
+
+console.log("Auth.js Loaded");
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcA1SCPkyoxrku-mD1_5GbPDSUfz8XW50",
+    authDomain: "finance-track-500fa.firebaseapp.com",
+    projectId: "finance-track-500fa",
+    storageBucket: "finance-track-500fa.firebasestorage.app",
+    messagingSenderId: "90116850858",
+    appId: "1:90116850858:web:a6733771feb65d711cbcd3",
+    measurementId: "G-21BEF5XHDM"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
